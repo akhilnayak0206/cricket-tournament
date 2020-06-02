f374c7025ba916eaf63063d40866bfebe3cc5b60
Akhil D Nayak <37106902+akhilnayak0206@users.noreply.github.com>
2020-06-02 13:41:44 +0530

    predeploy

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const connectDB = require('./config/db');
 
+const path = require('path');
+
 const app = express();
 
 //Connect Database
@@ -12,6 +14,16 @@ app.use(express.json({ extended: false }));
 
 //Define Routes
 app.use('/test', require('./routes/api/test')); //get count of countries in database
+
+// Serve static assets i.e. React(Front End) build folder in production
+if (process.env.NODE_ENV === 'production') {
+  //Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, './frontend/build/index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
